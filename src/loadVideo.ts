965f69746550ecc445e1e37f5ed224d7b5e9ec62
Gustavo A <gustavo.atencio@outlook.com>
2023-10-14 08:05:27 -0500

    typing master json

--- src/loadVideo.ts
+++ src/loadVideo.ts
@@ -1,10 +1,14 @@
+import { MasterVideo } from "./MasterVideo"
 import { Video } from "./Video"
 import { fetchWithRetry } from "./fetchWithRetry"
+//import url from "node:url"
 
 export async function loadVideo(video: Video) {
 	const masterUrl = new URL(video.url).toString()
 
 	const response = await fetchWithRetry({ url: masterUrl })
-	const master = await response.json()
-	console.log(master)
+	const master = await response.json() as MasterVideo
+
+	const videoParts = master.video.sort((a, b) => a.avg_bitrate - b.avg_bitrate)
+	const audioParts = master.audio?.sort((a, b) => a.avg_bitrate - b.avg_bitrate)
 }
